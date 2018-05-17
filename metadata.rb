6a5fc4cb3e6ad62bfd18742c8ae409242dea05be
James Casey <james@chef.io>
2018-05-17 08:32:55 -0700

    Add habitat dep

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures hab_national_parks'
 long_description 'Installs/Configures hab_national_parks'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -18,3 +18,4 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/hab_national_parks'
+depends 'habitat'
