08ba08ff8e1dce6352e3d58bc4cde2f340670dd6
James Casey <james@chef.io>
2018-07-16 14:35:45 -0700

    specify all the actions

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,10 +5,17 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 # set up with just the defaults
+hab_install 'install habitat'
 hab_sup 'default'
 
+hab_package 'jamesc/np-mongodb' do
+  channel 'stable'
+end
 hab_service 'jamesc/np-mongodb'
 
+hab_package 'jamesc/national-parks' do
+  channel 'stable'
+end
 hab_service 'jamesc/national-parks' do
     strategy 'at-once'
     bind 'database:np-mongodb.default'
